7a49bd8db7b50988cfbd8836651d47d1a9ceb96c
Breeze <akshadsatpute1989@gmail.com>
2026-07-09 16:11:57 +0530

    minor fixes

--- ronova/plugins/bot/send_dm.py
+++ ronova/plugins/bot/send_dm.py
@@ -8,12 +8,13 @@ from config import ADMIN_ID
 async def send_dm(c: Client, m: Message):
     await asyncio.sleep(0.2)
     if m.from_user.id != ADMIN_ID[0]:
-        await c.forward_messages(ADMIN_ID[0], m.chat.id, m.id)
+        x = await c.forward_messages(ADMIN_ID[0], m.chat.id, m.id)
     else:
         if m.from_user.id == ADMIN_ID[0]:
             if m.reply_to_message:
                 try:
                     r = m.reply_to_message.forward_origin.sender_user
-                    await c.send_message(r.id, m.text)
+                    await c.forward_messages(r.id, m.from_user.id, m.id, hide_sender_name=True)
+                    
                 except:
                     pass
