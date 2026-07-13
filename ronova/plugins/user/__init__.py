b705efd6d81fcc1844f6ae9f5d674d34c952b596
Breeze <akshadsatpute1989@gmail.com>
2026-07-13 12:20:24 +0530

    added afk to init

--- ronova/plugins/user/__init__.py
+++ ronova/plugins/user/__init__.py
@@ -1,9 +1,9 @@
 from . import (ani_search, delete, eval, just_google_it,
                logs, search_movie, search_word, shell,
-               start, stats, think, wiki)
+               start, stats, think, wiki, afk)
 
 __all__ :list[str] = [
     "ani_search", "delete", "eval", "just_google_it",
     "logs", "search_movie", "search_word", "shell",
-    "start", "stats", "think", "wiki"
+    "start", "stats", "think", "wiki", "afk"
 ]
