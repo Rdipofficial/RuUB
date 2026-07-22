fdae1eef7bedc3ddd041642002e962c2ff1fa4cc
Breeze <akshadsatpute1989@gmail.com>
2026-07-22 12:54:42 +0530

    prem filter

--- ronova/plugins/filters/__init__.py
+++ ronova/plugins/filters/__init__.py
@@ -1,4 +1,5 @@
 from .filterstarts import starts
 from .to_my_msg import to_me
+from .check_click import check
 
-__all__:list[str] = ["starts", "to_me"]
+__all__:list[str] = ["starts", "to_me", "check"]
